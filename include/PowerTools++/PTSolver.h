2f83752ebb463ba27fd174ae2a9c8b4455080b59
Hassan <hassan.hijazi@nicta.com.au>
2016-03-24 09:58:37 +1100

    updating to the right version of PowerTools, previeous version was defective

---
+++ include/PowerTools++/PTSolver.h
@@ -0,0 +1,44 @@
+//
+//  PTSolver.h
+//  PowerTools++
+//
+//  Created by Hassan on 30/01/2015.
+//  Copyright (c) 2015 NICTA. All rights reserved.
+//
+
+#ifndef __PowerTools____Solver__
+#define __PowerTools____Solver__
+
+#include <stdio.h>
+#include <PowerTools++/Model.h>
+#include <PowerTools++/IpoptProgram.h>
+#ifdef ENABLE_GUROBI
+#include <PowerTools++/GurobiProgram.h>
+#endif
+
+class PTSolver {
+    
+protected:
+    union {
+#ifdef ENABLE_GUROBI
+        GurobiProgram* grb_prog;
+#endif
+        IpoptProgram* ipopt_prog;
+    } prog;
+
+public:
+    SolverType                      _stype;
+    /** Constructor */
+    //@{
+    PTSolver();
+
+    PTSolver(Model* model, SolverType stype);
+    //@}
+    void set_model(Model* m);
+    
+    /* Destructor */
+    ~PTSolver();
+    
+    int run(int output, bool relax);
+};
+#endif /* defined(__PowerTools____Solver__) */
