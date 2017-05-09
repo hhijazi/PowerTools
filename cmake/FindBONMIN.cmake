9c84ca6566bfcb224f7b35caa118c5a6b4efb10f
Dan Gordon <dan.gordon@anu.edu.au>
2017-05-10 09:27:31 +1000

    WIP.

--- cmake/FindBONMIN.cmake
+++ cmake/FindBONMIN.cmake
@@ -1,15 +1,3 @@
-#set(BONMIN_INCLUDE_DIRS /home/kbestuzheva/Bonmin/Bonmin/src/Interfaces /home/kbestuzheva/Bonmin/CoinUtils/src /home/kbestuzheva/Bonmin/Osi/src/Osi /home/kbestuzheva/CoinIpopt/Ipopt/src/Common /home/kbestuzheva/CoinIpopt/Ipopt/src/Interfaces)
-#set(BONMIN_INCLUDE_DIRS /home/kbestuzheva/Bonmin-1.8.4/Bonmin/src/Interfaces /home/kbestuzheva/Bonmin-1.8.4/CoinUtils/src /home/kbestuzheva/Bonmin-1.8.4/Osi/src/Osi /home/kbestuzheva/Bonmin-1.8.4/Ipopt/src/Common /home/kbestuzheva/Bonmin-1.8.4/Ipopt/src/Interfaces /home/kbestuzheva/Bonmin-1.8.4/Ipopt/src/LinAlg)
-
-#set(BONMIN_LIBRARIES /home/kbestuzheva/Bonmin-1.8.4/Ipopt/src/Interfaces/libipopt.la /home/kbestuzheva/Bonmin-1.8.4/Cbc/src/libCbcSolver.la /home/kbestuzheva/Bonmin-1.8.4/Cbc/src/libCbc.la /home/kbestuzheva/Bonmin-1.8.4/Cgl/src/libCgl.la /home/kbestuzheva/Bonmin-1.8.4/Clp/src/OsiClp/libOsiClp.la /home/kbestuzheva/Bonmin-1.8.4/Clp/src/libClpSolver.la /home/kbestuzheva/Bonmin-1.8.4/Clp/src/libClp.la /home/kbestuzheva/Bonmin-1.8.4/Osi/src/Osi/libOsi.la /home/kbestuzheva/Bonmin-1.8.4/CoinUtils/src/libCoinUtils.la /home/kbestuzheva/Bonmin-1.8.4/src/CbcBonmin/libbonmin.la /home/kbestuzheva/Bonmin-1.8.4/Bonmin/lib/libbonmin.so)
-
-#set(BONMIN_INCLUDE_DIRS /home/kbestuzheva/Bonmin-1.8.4/include/coin )
-
-#set(BONMIN_LIBRARIES /home/kbestuzheva/Bonmin-1.8.4/Bonmin/lib/libbonmin.so /home/kbestuzheva/Bonmin-1.8.4/Bonmin/lib/libbonmin.la)
-#set(BONMIN_LIBRARIES "${BONMIN_LIBRARIES};m;pthread")
-
-##set(BONMIN_ROOT_DIR "$ENV{BONMIN_ROOT_DIR}" CACHE PATH "BONMIN root directory.")
-#set(BONMIN_ROOT_DIR "/usr/local" CACHE PATH "BONMIN root directory.")
 set(BONMIN_ROOT_DIR "/home/kbestuzheva/Bonmin-1.8.4" CACHE PATH "BONMIN root directory.")
 
 message("Looking for Bonmin in ${BONMIN_ROOT_DIR}")
